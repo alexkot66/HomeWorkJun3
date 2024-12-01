f7e3992a088e71698883088107e09eaf28f2e60e
Alexandr <a-koshkarov@mail.ru>
2024-12-01 20:55:55 +0500

    Result HW3 Task1 and Task2

--- src/main/java/person/Main.java
+++ src/main/java/person/Main.java
@@ -1,8 +1,19 @@
 package person;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+/*
+Задание 1: Создайте класс Person с полями name и age. Реализуйте сериализацию и десериализацию этого класса в файл.
+
+Задание 2: Используя JPA, создайте базу данных для хранения объектов класса Person.
+Реализуйте методы для добавления, обновления и удаления объектов Person.
+ */
 
 public class Main {
 
@@ -27,6 +38,47 @@ public class Main {
 
         Person person4 = readInFile();
         System.out.println(person4);
+
+        try(SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Person.class)
+                .buildSessionFactory()) {
+
+            // Создание сессии
+            Session session = sessionFactory.getCurrentSession();
+
+            // Начало транзации
+            session.beginTransaction();
+
+            // Создание объекта
+            Person person5 = new Person("Глеб", 22);
+            Person person6 = new Person("Коля", 24);
+            session.save(person5);
+            session.save(person6);
+            System.out.println("Object person save successfully");
+
+            // Чтение объекта из базы данных
+            Person retrievedPerson = session.get(Person.class, person5.getId());
+            System.out.println("Object person retrieved successfully");
+            System.out.println("Retrieved person object: " + retrievedPerson);
+
+            // Обновление объекта
+            retrievedPerson.setName("Николай");
+            retrievedPerson.setAge(40);
+            session.update(retrievedPerson);
+            System.out.println("Object person update successfully");
+
+            // Удаление данных
+            //session.delete(retrievedPerson);
+            //System.out.println("Object person delete successfully");
+
+            session.getTransaction().commit();
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
--- src/main/java/person/Person.java
+++ src/main/java/person/Person.java
@@ -1,13 +1,32 @@
 package person;
 
+import javax.persistence.*;
+
 import java.io.Serializable;
 
+@Entity
+@Table(name = "students")
 public class Person implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "age")
     private int age;
 
+    public Person() {
+
+    }
+
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
         this.name = name;
         this.age = age;
     }
@@ -18,6 +37,17 @@ public class Person implements Serializable {
 
     public int getAge() {
         return age;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     @Override
